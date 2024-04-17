d76a59680ec28ad68193f5a5166c272da24fa1f5
nikhillohra <nikhillohra99@gmail.com>
2024-04-18 00:02:49 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./assets/Header"
+import './App.css'
+
+
+function App() {
+ 
+  return (
+    <>
+    <div className="head">
+    <Header/>
+    </div>
+    </>
+  )
+}
+
+export default App
